d33abad67d17489278ac6771d057a925c58ea3d0
Mark Wirblich <mark@wirblich.com>
2015-02-17 10:07:19 +0100

    Further package setup

---
+++ README.rst
@@ -0,0 +1,7 @@
+Piptree
+--------
+
+To use (with caution), simply do::
+
+    >>> import piptree
+    >>> piptree.show()
--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 from setuptools import setup
 
-setup(name='piptree',
-      version='0.1',
-      description='List your ',
-      url='http://github.com/mightym/piptree',
-      author='Mark Wirblich',
-      author_email='mark@wirblich.com',
-      license='MIT',
-      packages=['piptree'],
-      zip_safe=False)
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='piptree',
+    version='0.1',
+    description='List your ',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    keywords='pip requirements tree',
+    url='http://github.com/mightym/piptree',
+    author='Mark Wirblich',
+    author_email='mark@wirblich.com',
+    license='MIT',
+    packages=['piptree'],
+    install_requires=[
+        'pip',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    scripts=['bin/piptree'],
+    )
